c02c220f0ba6865d9963975a1afdb69ae970bf9e
Emre Bilal <emre_bilal21@hotmail.com>
2022-04-29 00:27:24 +0300

    add .

---
+++ CQRSMediatRExample/DAL/Queries/Request/GetByIdProductRequest.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Data;
+using DAL.Queries.Response;
+using MediatR;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DAL.Queries.Request
+{
+    public class GetByIdProductRequest : IRequest<GetByIdProductResponse>
+    {
+        public Guid Id { get; set; }
+    }
+
+    //Handler
+    public class GetByIdProductHandler : IRequestHandler<GetByIdProductRequest, GetByIdProductResponse>
+    {
+        public async Task<GetByIdProductResponse> Handle(GetByIdProductRequest request, CancellationToken cancellationToken)
+        {
+            var product = ApplicationDbContext.ProductList.FirstOrDefault(p => p.Id == request.Id);
+            if (product == null)
+                return null;
+
+            return new GetByIdProductResponse
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Quantity = product.Quantity,
+                CreateTime = product.CreateTime
+            };
+        }
+    }
+}
