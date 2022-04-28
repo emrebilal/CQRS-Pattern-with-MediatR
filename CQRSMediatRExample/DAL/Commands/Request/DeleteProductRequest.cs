5c11b98c342890f888680203a58b0075349e818d
Emre Bilal <emre_bilal21@hotmail.com>
2022-04-29 00:06:56 +0300

    add .

---
+++ CQRSMediatRExample/DAL/Commands/Request/DeleteProductRequest.cs
@@ -0,0 +1,35 @@
+﻿using DAL.Commands.Response;
+using DAL.Data;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DAL.Commands.Request
+{
+    public class DeleteProductRequest : IRequest<DeleteProductResponse>
+    {
+        public Guid Id { get; set; }
+    }
+
+    //Handler
+    public class DeleteProductHandler : IRequestHandler<DeleteProductRequest, DeleteProductResponse>
+    {
+        public async Task<DeleteProductResponse> Handle(DeleteProductRequest request, CancellationToken cancellationToken)
+        {
+            var deleteProduct = ApplicationDbContext.ProductList.FirstOrDefault(p => p.Id == request.Id);
+            if (deleteProduct == null)
+                return null;
+
+            ApplicationDbContext.ProductList.Remove(deleteProduct);
+
+            return new DeleteProductResponse
+            {
+                IsSuccess = true
+            };
+        }
+    }
+}
---
+++ CQRSMediatRExample/DAL/Commands/Response/CreateProductResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DAL.Commands.Response
+{
+    public class CreateProductResponse
+    {
+        public bool IsSuccess { get; set; }
+        public Guid ProductId { get; set; }
+    }
+}
